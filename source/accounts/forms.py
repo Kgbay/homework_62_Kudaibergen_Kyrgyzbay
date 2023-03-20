f85bf2d8d837fae2bbc542178b5ab7b9d0ba9ec1
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-03-21 05:04:00 +0600

    Добавил форму для регистрации новых пользователей

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -8,3 +9,36 @@ class LoginForm(forms.Form):
 
     username.widget.attrs.update({'class': 'form-control form-control-lg'})
     password.widget.attrs.update({'class': 'form-control form-control-lg'})
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Пароль',
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput
+    )
+    password_confirm = forms.CharField(
+        label='Подтвердите пароль',
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirm', 'first_name', 'last_name', 'email')
+
+    def  clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
